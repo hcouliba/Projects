b5564289ad168b43f2f482a23b1a43144284ecc3
hcouliba <hcouliba@gwu.edu>
2021-11-05 13:57:23 -0400

    Update FDA_Dataset_Parsing.py

--- OncoMX/FDA_Dataset_Parsing.py
+++ OncoMX/FDA_Dataset_Parsing.py
@@ -1,30 +1,27 @@
 #%%
+#from numpy.random.mtrand import f
+#import matplotlib.pyplot as plt
+#from matplotlib.patches import Polygon
+#from pandas.core.indexing import convert_to_index_sliceable
+#import seaborn as sns
+#plt.style.use('classic') 
+#sns.set(style="ticks")
 import os
-import numpy as np
-from numpy.random.mtrand import f
+from glob import glob
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-import seaborn as sns
-import glob
 
-plt.style.use('classic') 
-sns.set(style="ticks")
 # %% 
+FDA_portfolio = ['dataset_breast.csv','dataset_colorectal.csv','dataset_lung.csv','dataset_melanoma.csv','dataset_ovarian.csv','dataset_prostate.csv']
+#from pathlib import Path
 # Path to FDA biomarker datasets folder
-path = r'/Users/hawacoulibaly/Documents/HIVE Lab/OncoMX/Datasets/FDA_Dataset'
+path = '/Users/hawacoulibaly/Documents/GitHub/HIVE-Lab/OncoMX/FDA_Dataset'
+import glob
 FDA_dataset = glob.glob(path + "/*.csv") # All datasets combined (melanoma, prostate, ovarian, lung, colorectal, and breast)
 
 # %%
-#Define parsing function to return
-def parse_csv(filename):
-        df = pd.read_csv(filename, index_col=None, header=0)
-        for col in df.columns:
-                print(col, '\n,', filename)
-
+for i in FDA_dataset: 
+        df = pd.read_csv(i, index_col=None, header=0) # Create dataframe
+        for col in list(df.columns): 
+                print(col,',',os.path.basename(i).replace('.csv','')) # Return column name and dataset name                
 # %%
-# Apply function to dataset
-for x in FDA_dataset:
-        print(parse_csv(x))
 
-# ##
