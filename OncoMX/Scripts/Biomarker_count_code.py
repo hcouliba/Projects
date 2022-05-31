22f91460134ed443cf91f4699923b1d4186adf7e
hcouliba <hcouliba@gwu.edu>
2022-05-31 12:48:41 -0400

    Delete

--- OncoMX/Scripts/Biomarker_count_code.py
+++ OncoMX/Scripts/Biomarker_count_code.py
@@ -1,4 +1,5 @@
 #%%
+# Script to clean biomarker datasets at https://data.oncomx.org for biomarker count
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -41,25 +42,21 @@ def Dataset(dframe, valCnt = False):
       print(dframe[colname].value_counts())
       count +=1
 
-# 
-df = pd.read_csv(r'/Users/hawacoulibaly/Downloads/human_cancer_biomarker_panel_prosigna_breast_cancer.csv' , index_col=0)
+# Upload dataset using absolute pathname
+df = pd.read_csv(r'/Users/hawacoulibaly/Downloads/Cancer Biomarkers v1.0 - Reviewed Biomarkers.csv' , index_col=0)
 
 Dataset(df, True)
 # %% 
+# Check for Disease name column values
+#df = df.rename(columns={'Disease name': 'Disease'})
+#df = df.rename(columns={'BEST biomarker type': 'BEST'})
+#df = df.rename(columns={'Assessed biomarker entity': 'entity'})
 
-df = df.rename(columns={'Disease name': 'Disease'})
-df = df.rename(columns={'BEST biomarker type': 'BEST'})
-df = df.rename(columns={'Assessed biomarker entity': 'entity'})
-
-#df['Disease'].value_counts()
-#df['BEST biomarker type'].value_counts()
-
-# %%
-print(df.Disease.describe(), '\n', df.Disease.value_counts(dropna=False))
-
+df['Disease name'].value_counts()
 print("\nReady to continue.")
 # %% 
-df['Disease'] = df['Disease'].replace({'ovarian cancer (DOID: 2394)':'ovarian cancer (DOID:2394)',\
+# For the specific column, standardize disease names by replacing mispellings and other inconsistencies
+df['Disease name'] = df['Disease name'].replace({'ovarian cancer (DOID: 2394)':'ovarian cancer (DOID:2394)',\
                                         'Lung cancer (DOID:1324)' : 'lung cancer (DOID:1324)',\
                                         'lung cancer (DO:1324)':'lung cancer (DOID:1324)',\
                                         'lung cancer (DO1324)':'lung cancer (DOID:1324)',\
@@ -76,6 +73,8 @@ df['Disease'] = df['Disease'].replace({'ovarian cancer (DOID: 2394)':'ovarian ca
                                         'Head and Neck Cancer (DOID:11934)':'head and neck cancer (DOID:11934)', \
                                         'Stomach cancer (DOID:10534)':'stomach cancer (DOID:10534)', \
                                         'Stomach cancer(DOID:10534)':'stomach cancer (DOID:10534)', \
+                                        'Stomach Cancer(DOID:10534)':'stomach cancer (DOID:10534)', \
+                                        'Stomach cancer(DOID:10534)  ':'stomach cancer (DOID:10534)', \
                                         'Melanoma (DOID:1909)':'melanoma (DOID:1909)',\
                                         'melanoma (DOID:4159)':'melanoma (DOID:1909)',\
                                         'Liver cancer(DOID:3571)':'liver cancer (DOID:3571)',\
@@ -83,62 +82,23 @@ df['Disease'] = df['Disease'].replace({'ovarian cancer (DOID: 2394)':'ovarian ca
                                         'Urinary bladder cancer (DOID:11054)':'urinary bladder cancer (DOID:11054)',\
                                         'Uterine Cancer':'uterine cancer (DOID:363)',\
                                         'Kidney cancer (DOID:263)':'kidney cancer (DOID:263)',\
+                                        'Kidney cancer (DOID:263) ':'kidney cancer (DOID:263)',\
+                                        'esophageal cancer (DOID:5041':'esophageal cancer (DOID:5041)',\
                                         'Hepatocellular carcinoma (DOID:684)': 'liver cancer (DOID:3571)'})
 
+print("\nReady to continue.")
 # %%
-print(df.Disease.describe(), '\n', df.Disease.value_counts(dropna=False))
+# Verify that all values are standardized
 
+print(df['Disease name'].value_counts())
+
+print("\nReady to continue.")
 # %%
+# Biomarker count per disease (cancer type) : BAR CHART
 from matplotlib import cm
 color = cm.Blues_r(np.linspace(.4, .8, 30))
-df.Disease.value_counts().plot(kind='bar', stacked=True, color=color, title='Biomarker Count per Cancer Type ', grid=False, figsize=(15,7))
-# %%
-#%%
-from matplotlib import cm
-color = cm.inferno_r(np.linspace(.4, .8, 30))
-df.Disease.value_counts().plot(kind='bar', stacked=True, color=color, title='Biomarker Count per Cancer Type ', grid=False, figsize=(15,7))
-# %% 
-# Count total number of rows 
-index = df.index
-number_of_rows = len(index)
-print(number_of_rows)
-# %%
-print(df.nunique())
-# %%
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
-
-# Use the venn2 function
-venn2(subsets = (258, 0, 213), set_labels = ('', ''))
-plt.show()
-# %%
-df = pd.read_csv(r'/Users/hawacoulibaly/Downloads/Cancer Biomarkers v1.0 - Reviewed Biomarkers.csv' , index_col=0)
 df = df.rename(columns={'Disease name': 'Disease'})
-index = df.index
-number_of_rows = len(index)
-print(number_of_rows)
-# %%
-print(df.nunique())
+df.Disease.value_counts().plot(kind='bar', stacked=True, color=color, title='Biomarker Count per Cancer Type ', grid=False, figsize=(15,7))
 
-
-# %%
-# Prepare data
-x_var = 'Disease'
-groupby_var = 'BEST'
-df_agg = df.loc[:, [x_var, groupby_var]].groupby(groupby_var)
-vals = [df[x_var].values.tolist() for i, df in df_agg]
-
-# Draw
-plt.figure(figsize=(16,9), dpi= 80)
-colors = [plt.cm.Spectral(i/float(len(vals)-1)) for i in range(len(vals))]
-n, bins, patches = plt.hist(vals, df[x_var].unique().__len__(), stacked=True, density=False, color=colors[:len(vals)])
-
-# Decoration
-plt.legend({group:col for group, col in zip(np.unique(df[groupby_var]).tolist(), colors[:len(vals)])})
-plt.title(f"Stacked Histogram of ${x_var}$ colored by ${groupby_var}$", fontsize=22)
-plt.xlabel(x_var)
-plt.ylabel("Frequency")
-plt.ylim(0, 200)
-plt.xticks(ticks=bins, labels=np.unique(df[x_var]).tolist(), rotation=90, horizontalalignment='left')
-plt.show()
+print("\nReady to continue.")
 # %%
