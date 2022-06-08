24e72fe867997ae2658297afe49eafc4a47fd205
hcouliba <hcouliba@gwu.edu>
2022-06-08 14:23:46 -0400

    Update Cancer_Biomarker_count_code.py

--- OncoMX/Data analysis/Cancer_Biomarker_count_code.py
+++ OncoMX/Data analysis/Cancer_Biomarker_count_code.py
@@ -1,5 +1,10 @@
 #%%
-# Script to clean biomarker datasets at https://data.oncomx.org for biomarker count
+# 
+"""
+Script to clean biomarker QCed datasets at https://data.oncomx.org for bi-weekly 
+biomarker count
+
+"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
